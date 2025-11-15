2a248b94aaebb5c13de28ffaa3172ed4ba522535
mchisxyz <diepminhchi1617@gmail.com>
2025-11-15 15:38:17 +0100

    init

---
+++ src/main/java/vn/diepgia/mchis/currency_rate_fetcher/CurrencyRateFetcherApplication.java
@@ -0,0 +1,13 @@
+package vn.diepgia.mchis.currency_rate_fetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyRateFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyRateFetcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/diepgia/mchis/currency_rate_fetcher/CurrencyRateFetcherApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.diepgia.mchis.currency_rate_fetcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyRateFetcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
